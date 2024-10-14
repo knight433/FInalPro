e97b9e67c78ddbc304fdde8b4291465c090e622d
knight433 <dhruvanu3@gmail.com>
2024-10-14 19:46:32 +0530

    basic page

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, Response, url_for, request, redirect, jsonify
-from flask_socketio import SocketIO, emit
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-socket = SocketIO(app)
 
 @app.route('/')
 def home():
-    return render_template('basic.html')
+    return render_template('index.html')
+
+@app.route('/submit-text', methods=['POST'])
+def submit_text():
+    data = request.get_json()
+    user_text = data.get('userText', '')
+    response_message = f'Received: {user_text}'
+    return jsonify({'message': response_message})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ webapp/backend.py
@@ -0,0 +1,9 @@
+class YTsumer:
+    def __init__(self):
+        pass
+
+    def getLink(self,link): #download the vid
+        pass
+
+    def getsummart(self): #return the summary
+        pass
