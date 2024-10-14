8359bd02afbd0e9682a86db8730d81fb72d0c434
knight433 <dhruvanu3@gmail.com>
2024-10-14 21:55:11 +0530

    added webpage functions

--- webapp/app.py
+++ webapp/app.py
@@ -1,18 +1,29 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template, jsonify
+from backend import Backend 
 
 app = Flask(__name__)
+backend = Backend()
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/submit-text', methods=['POST'])
-def submit_text():
-    data = request.get_json()
-    user_text = data.get('userText', '')
-    response_message = f'Received: {user_text}'
-    return jsonify({'message': response_message})
+@app.route('/process_video', methods=['POST'])
+def process_video():
+    video_link = request.form.get('link')
+    
+    if not video_link:
+        return render_template('index.html', error="No link provided")
+    
+    backend.video_to_mp3(video_link)
+    summary = backend.sum_up()
+    
+    if summary:
+        # Pass the title along with the summary to the template
+        return render_template('index.html', summary=summary, title=backend.video_title, link=video_link)
+    else:
+        return render_template('index.html', error="Failed to summarize the video")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
